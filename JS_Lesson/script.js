fdc0a4117dc9708a8fad0c9dc1b955e13d459e68
tomaschukanya-debug <tomaschukanya@gmail.com>
2026-03-31 14:13:56 +0300

    Add files via upload

---
+++ JS_Lesson/script.js
@@ -0,0 +1,9 @@
+// Створюємо змінні
+let name = "Аліна";
+let age = 20;
+let isStudent = true;
+
+// Виводимо змінні в консоль
+console.log("Ім’я:", name);
+console.log("Вік:", age);
+console.log("Студент:", isStudent);
